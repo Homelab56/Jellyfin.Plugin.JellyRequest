11d4dc6cf4335a4114d1ff4a87a0feceb21a9967
Glenn <glenn@homelab.local>
2026-04-22 22:57:36 +0200

    Simplify Plugin.cs to minimal working version
    - Remove unused dependencies and services
    - Simplify to single web page (index.html)
    - Remove dependency injection for now
    - Focus on basic plugin loading first

--- Plugin.cs
+++ Plugin.cs
@@ -1,50 +1,27 @@
+cat <<EOF > Plugin.cs
 using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Reflection;
-using Jellyfin.Plugin.JellyRequest.Data;
 using Jellyfin.Plugin.JellyRequest.Configuration;
-using Jellyfin.Plugin.JellyRequest.Services;
 using MediaBrowser.Common.Configuration;
 using MediaBrowser.Common.Plugins;
 using MediaBrowser.Model.Plugins;
 using MediaBrowser.Model.Serialization;
-using Microsoft.Extensions.DependencyInjection;
-
 
 namespace Jellyfin.Plugin.JellyRequest
 {
-    /// <summary>
-    /// The main plugin entry point for JellyRequest.
-    /// </summary>
     public class Plugin : BasePlugin<PluginConfiguration>, IHasWebPages
     {
-        /// <summary>
-        /// Initializes a new instance of the <see cref="Plugin"/> class.
-        /// </summary>
-        /// <param name="applicationPaths">Instance of the <see cref="IApplicationPaths"/> interface.</param>
-        /// <param name="xmlSerializer">Instance of the <see cref="IXmlSerializer"/> interface.</param>
+        public override string Name => "JellyRequest";
+        public override Guid Id => Guid.Parse("c8b5e8a2-4d7c-4e9f-8a1b-2c3d4e5f6a7b");
+
         public Plugin(IApplicationPaths applicationPaths, IXmlSerializer xmlSerializer)
             : base(applicationPaths, xmlSerializer)
         {
             Instance = this;
         }
 
-        /// <summary>
-        /// Gets the current plugin instance.
-        /// </summary>
-        public static Plugin? Instance { get; private set; }
+        public static Plugin Instance { get; private set; }
 
-        /// <inheritdoc />
-        public override string Name => "JellyRequest";
-
-        /// <inheritdoc />
-        public override string Description => "Netflix-style content discovery and request system for Jellyfin.";
-
-        /// <inheritdoc />
-        public override Guid Id => Guid.Parse("c8b5e8a2-4d7c-4e9f-8a1b-2c3d4e5f6a7b");
-
-        /// <inheritdoc />
         public IEnumerable<PluginPageInfo> GetPages()
         {
             return new[]
@@ -52,30 +29,10 @@ namespace Jellyfin.Plugin.JellyRequest
                 new PluginPageInfo
                 {
                     Name = "JellyRequest",
-                    EmbeddedResourcePath = GetType().Namespace + ".Web.discover.html"
-                },
-                new PluginPageInfo
-                {
-                    Name = "JellyRequestConfig",
-                    EmbeddedResourcePath = GetType().Namespace + ".Configuration.config.html"
-                },
-                new PluginPageInfo
-                {
-                    Name = "MyRequests",
-                    EmbeddedResourcePath = GetType().Namespace + ".Web.myrequests.html"
+                    EmbeddedResourcePath = GetType().Namespace + ".Web.index.html"
                 }
             };
         }
-
-        public void ConfigureServices(IServiceCollection serviceCollection)
-        {
-            serviceCollection.AddHostedService<PollingService>();
-            serviceCollection.AddSingleton<TmdbService>();
-            serviceCollection.AddSingleton<RadarrService>();
-            serviceCollection.AddSingleton<SonarrService>();
-            serviceCollection.AddSingleton<LibraryMatchService>();
-            serviceCollection.AddSingleton<RequestRepository>();
-            serviceCollection.AddSingleton<RequestService>();
-        }
     }
 }
+EOF
